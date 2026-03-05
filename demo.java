b69dc1e7ea678b57f1e2d3d5cc9722ef2da77fcd
Poorvik Gowda N <poorvikgowdapoorvik251@gmail.com>
2026-03-05 12:40:33 +0530

    Add additional print statements to demo class

--- demo.java
+++ demo.java
@@ -1,5 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         System.out.println("hii hello");
+        System.out.println("bye");
+        System.out.println("run");
     }
 }
